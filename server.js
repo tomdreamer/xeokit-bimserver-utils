840ccd586e0e59d03175ecda22a6a6b40394c7d1
xeolabs <lindsay.kay@xeolabs.com>
2019-02-07 16:52:35 +0100

    Code comments

--- server.js
+++ server.js
@@ -3,20 +3,27 @@ import express from 'express'
 const app = express();
 const fs = require('fs');
 
-//
-
 import BimServerClient from "./lib/bimserver/BIMServerClient/bimserverclient.js";
-import loadBIMServerMetaModelJSON from "./lib/bimserver/loadBIMServerMetaModelJSON.js";
+import {loadBIMServerMetaModelJSON} from "./lib/bimserver/loadBIMServerMetaModelJSON.js";
 
-//
+//---------------------------------------------------------------------------------------------------------------------
+// Configurations - tweak these as necessary.
+//---------------------------------------------------------------------------------------------------------------------
 
+// BIMServer login credentials
 const bimServerAddress = "https://xeokit.cleverapps.io/";
 const username = "kevin.flynn@en.com";
 const password = "secretPassword";
-const poid = 196609;
 
-const modelId = "myModel";
-const outputFilePath = "./output/myMetaModel.json";
+const poid = 196609; // BIMServer project whose latest revision we'll get metadata for
+
+const modelId = "myModel"; // This model ID gets included iin the metadata
+const outputFilePath = "./output/myMetaModel.json"; // Where we'll write the metadata JSON file
+
+//---------------------------------------------------------------------------------------------------------------------
+// Log into BIMServer and download JSON metadata for the latest revision
+// of the project given in the configurations above.
+//---------------------------------------------------------------------------------------------------------------------
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
 
